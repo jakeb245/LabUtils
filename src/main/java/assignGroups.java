f05dfa791be6f6230cee3a05a6c2ffdf32c0815b
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-08-28 16:04:29 -0400

    Testing stuff

--- src/main/java/assignGroups.java
+++ src/main/java/assignGroups.java
@@ -1,12 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class assignGroups {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner filenameIn = new Scanner(System.in);
 
         System.out.print("Enter roster filename: ");
-        String rosterName = in.nextLine();
+        String rosterName = String.valueOf(Paths.get(filenameIn.nextLine()));
+        ArrayList<String> names = readNamesFromCSV(rosterName);
+        System.out.println(names);
+    }
 
-        System.out.println(rosterName);
+    public static ArrayList<String> readNamesFromCSV(String filename) throws IOException {
+        ArrayList<String> names = new ArrayList<>();
+
+        Scanner in = new Scanner(Paths.get(filename));
+
+        while (in.hasNextLine()) {
+            System.out.println("line!");
+            in.nextLine();
+        }
+
+        return names;
     }
 }
