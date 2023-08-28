b59224b1b93b2b8b72372c559a869fc23efda363
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-08-28 15:30:57 -0400

    Start

---
+++ src/main/java/assignGroups.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class assignGroups {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter roster filename: ");
+        String rosterName = in.nextLine();
+
+        System.out.println(rosterName);
+    }
+}
